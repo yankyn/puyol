f502cb94e754dd1d2744b6583b0e2eada579eb1d
yankyn <yankyn@gmail.com>
2014-05-26 00:17:21 +0300

    Test refactoring and some added functionality.

--- src/puyol/orm/orm.py
+++ src/puyol/orm/orm.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Integer, Column, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
 from puyol.orm.base import AlchemyBase, Base
 
@@ -11,6 +12,11 @@ class Country(Base, AlchemyBase):
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
+    universities = relationship('University')
+
+    def foo(self):
+        pass
+
 
 class University(Base, AlchemyBase):
     __tablename__ = 'universities'
@@ -18,6 +24,9 @@ class University(Base, AlchemyBase):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     country_id = Column(ForeignKey(Country.id))
+
+    country = relationship('Country', uselist=False)
+    courses = relationship('Course')
 
 
 class Student(Base, AlchemyBase):
@@ -33,6 +42,8 @@ class Course(Base, AlchemyBase):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     university_id = Column(ForeignKey(University.id))
+
+    university = relationship('University')
 
 
 class StudentCourseRelations(Base, AlchemyBase):
---
+++ src/setup.py
@@ -0,0 +1,10 @@
+__author__ = 'Nathaniel'
+
+import setuptools
+from setuptools import setup
+
+setup(name='puyol',
+      version='0.11',
+      description='Sqlalchemy ORM mock',
+      packages=['puyol', 'puyol_tests'],
+     )
